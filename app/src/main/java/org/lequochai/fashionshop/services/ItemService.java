f97a78d212a6de6e04ed2580c6e00f49a98a96bb
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-12 22:14:41 +0700

    Modified ItemService.

--- app/src/main/java/org/lequochai/fashionshop/services/ItemService.java
+++ app/src/main/java/org/lequochai/fashionshop/services/ItemService.java
@@ -7,8 +7,15 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ItemService {
     @GET ("/item?method=getAll")
     Call<RestfulResponse<List<Item>>> getAll();
+
+    @GET ("/item?method=getByKeyword")
+    Call<RestfulResponse<List<Item>>> getByKeyword(@Query( "keyword" ) String keyword);
+
+    @GET ("/item?method=get")
+    Call<RestfulResponse<Item>> get(@Query( "id" ) String id);
 }
