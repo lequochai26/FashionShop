20b224a61d9dbb665252c0645ae5e5d430b369d6
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-13 01:31:54 +0700

    Added ItemsListItem activity.

---
+++ app/src/main/java/org/lequochai/fashionshop/ItemsListItem.java
@@ -0,0 +1,14 @@
+package org.lequochai.fashionshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemsListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_list_item);
+    }
+}
