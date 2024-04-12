5cacd0db079b354756125f73ae1aa2bbbfbcc8a6
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-13 01:46:15 +0700

    Added MainActivityController abstract class.

---
+++ app/src/main/java/org/lequochai/fashionshop/controllers/MainActivityController.java
@@ -0,0 +1,10 @@
+package org.lequochai.fashionshop.controllers;
+
+import org.lequochai.fashionshop.MainActivity;
+
+public abstract class MainActivityController<P> extends ViewController<MainActivity, P> {
+//    Constructors:
+    public MainActivityController(MainActivity view) {
+        super(view);
+    }
+}
