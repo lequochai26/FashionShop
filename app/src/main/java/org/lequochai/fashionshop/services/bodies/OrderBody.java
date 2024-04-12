46f32b9dac98b93d8e359434abc6bd7de8fb1606
ThanhHieu789 <neonkoro789@gmail.com>
2024-04-13 00:39:31 +0700

    update orderbody

--- app/src/main/java/org/lequochai/fashionshop/services/bodies/OrderBody.java
+++ app/src/main/java/org/lequochai/fashionshop/services/bodies/OrderBody.java
@@ -1,43 +1,53 @@
 package org.lequochai.fashionshop.services.bodies;
 
-import org.lequochai.fashionshop.entities.User;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public class OrderBody {
-    private String id;
-    private String type;
-    private Date date;
-    private Number totalPrice;
-    private String metadata;
-    private User createdBy;
-    private User orderedBy;
-    private List<String> orders;
-    private String status;
-    private String paymentMethod;
 
-    public  OrderBody(){}
+public static  class User{
+    private String email;
+    private  String fullName;
 
-    public OrderBody(String id, String type, Date date, Number totalPrice, String metadata, User createdBy, User orderedBy, List<String> orders, String status, String paymentMethod) {
-        this.id = id;
+    public  User(){}
+
+    public User(String email, String fullName) {
+        this.email = email;
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
+   private  String type;
+
+   private Number totalPrice;
+   private User orderedBy;
+   private List<Map<String,Object>> items;
+
+   private String paymentMethod;
+
+    public OrderBody(String type, Number totalPrice, User orderedBy, List<Map<String, Object>> items, String paymentMethod) {
         this.type = type;
-        this.date = date;
         this.totalPrice = totalPrice;
-        this.metadata = metadata;
-        this.createdBy = createdBy;
         this.orderedBy = orderedBy;
-        this.orders = orders;
-        this.status = status;
+        this.items = items;
         this.paymentMethod = paymentMethod;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getType() {
@@ -48,36 +58,12 @@ public class OrderBody {
         this.type = type;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
     public Number getTotalPrice() {
         return totalPrice;
     }
 
     public void setTotalPrice(Number totalPrice) {
         this.totalPrice = totalPrice;
-    }
-
-    public String getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(String metadata) {
-        this.metadata = metadata;
-    }
-
-    public User getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(User createdBy) {
-        this.createdBy = createdBy;
     }
 
     public User getOrderedBy() {
@@ -88,20 +74,12 @@ public class OrderBody {
         this.orderedBy = orderedBy;
     }
 
-    public List<String> getOrders() {
-        return orders;
+    public List<Map<String, Object>> getItems() {
+        return items;
     }
 
-    public void setOrders(List<String> orders) {
-        this.orders = orders;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+    public void setItems(List<Map<String, Object>> items) {
+        this.items = items;
     }
 
     public String getPaymentMethod() {
