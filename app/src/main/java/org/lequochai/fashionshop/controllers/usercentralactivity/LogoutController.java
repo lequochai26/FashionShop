3776adf68bf695b35e8ab4b0db28bec828adae24
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-14 01:48:31 +0700

    Modified BuyController to check non-login case.

---
+++ app/src/main/java/org/lequochai/fashionshop/controllers/usercentralactivity/LogoutController.java
@@ -0,0 +1,16 @@
+package org.lequochai.fashionshop.controllers.usercentralactivity;
+
+import org.lequochai.fashionshop.UserCentralActivity;
+
+public class LogoutController extends UserCentralActivityController<Void> {
+//    Constructors:
+    public LogoutController(UserCentralActivity view) {
+        super(view);
+    }
+
+//    Methods:
+    @Override
+    public void execute(Void param) {
+
+    }
+}
