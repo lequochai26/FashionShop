783f8c68c13f40f26d69e10ccef8b9cc43bc01a6
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-14 13:38:19 +0700

    Added LoadOrderedOrdersController.

--- app/src/main/java/org/lequochai/fashionshop/OrderedOrderActivity.java
+++ app/src/main/java/org/lequochai/fashionshop/OrderedOrderActivity.java
@@ -3,12 +3,71 @@ package org.lequochai.fashionshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.lequochai.fashionshop.adapters.OrdersListViewItemAdapter;
+import org.lequochai.fashionshop.controllers.Controller;
+import org.lequochai.fashionshop.controllers.orderedordersactivity.LoadOrderedOrdersController;
+import org.lequochai.fashionshop.entities.Order;
+
+import java.util.List;
 
 public class OrderedOrderActivity extends AppCompatActivity {
+//    Fields:
+    private ImageView btnBack;
+    private TextView lblTitle;
+    private ListView ordersListView;
 
+    private Controller<Void> loadOrderedOrdersController;
+
+//    Constructors:
+    public OrderedOrderActivity() {
+
+    }
+
+//    Creation method:
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordered_order);
+
+        getViews();
+
+        initialControllers();
+
+        setupViews();
+
+        init();
+    }
+
+//    Setup methods:
+    private void getViews() {
+        btnBack = findViewById(R.id.btnBack4);
+        lblTitle = findViewById(R.id.lblTitle);
+        ordersListView = findViewById(R.id.ordersListView);
+    }
+
+    private void initialControllers() {
+        loadOrderedOrdersController = new LoadOrderedOrdersController(this);
+//        TODO
+    }
+
+    private void setupViews() {
+        btnBack.setOnClickListener(
+                t -> finish()
+        );
+    }
+
+//    Init:
+    private void init() {
+        loadOrderedOrdersController.execute(null);
+    }
+
+//    Methods:
+    public void loadOrders(List<Order> orders) {
+        OrdersListViewItemAdapter adapter = new OrdersListViewItemAdapter(this, orders);
+        ordersListView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/org/lequochai/fashionshop/controllers/orderedordersactivity/LoadOrderedOrdersController.java
@@ -0,0 +1,51 @@
+package org.lequochai.fashionshop.controllers.orderedordersactivity;
+
+import org.lequochai.fashionshop.OrderedOrderActivity;
+import org.lequochai.fashionshop.entities.Order;
+import org.lequochai.fashionshop.response.RestfulResponse;
+import org.lequochai.fashionshop.services.GlobalService;
+import org.lequochai.fashionshop.utils.DialogHelper;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoadOrderedOrdersController extends OrderedOrdersActivityController<Void> {
+//    Constructors:
+    public LoadOrderedOrdersController(OrderedOrderActivity view) {
+        super(view);
+    }
+
+//    Methods:
+    @Override
+    public void execute(Void param) {
+        GlobalService.getInstance(view)
+                .getOrderService()
+                .getOrdered()
+                .enqueue(
+                        new Callback<RestfulResponse<List<Order>>>() {
+                            @Override
+                            public void onResponse(Call<RestfulResponse<List<Order>>> call, Response<RestfulResponse<List<Order>>> response) {
+                                if (response.isSuccessful()) {
+                                    RestfulResponse<List<Order>> body = response.body();
+
+                                    if (body.isSuccess()) {
+                                        view.loadOrders(body.getResult());
+                                    }
+                                    else {
+                                        DialogHelper.showAlertDialog(view, body.getCode(),
+                                                body.getMessage());
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<RestfulResponse<List<Order>>> call, Throwable throwable) {
+                                DialogHelper.showErrorDialog(view, throwable.toString());
+                            }
+                        }
+                );
+    }
+}
