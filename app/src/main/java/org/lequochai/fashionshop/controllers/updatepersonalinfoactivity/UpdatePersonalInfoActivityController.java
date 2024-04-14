270ac348bd8682880b0f0bff05231f8d6308663c
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-14 11:21:26 +0700

    Added UpdatePersonalInfoActivityController.

---
+++ app/src/main/java/org/lequochai/fashionshop/controllers/updatepersonalinfoactivity/UpdatePersonalInfoActivityController.java
@@ -0,0 +1,11 @@
+package org.lequochai.fashionshop.controllers.updatepersonalinfoactivity;
+
+import org.lequochai.fashionshop.UpdatePersonalInfoActivity;
+import org.lequochai.fashionshop.controllers.ViewController;
+
+public abstract class UpdatePersonalInfoActivityController<P> extends ViewController<UpdatePersonalInfoActivity, P> {
+//    Constructors:
+    public UpdatePersonalInfoActivityController(UpdatePersonalInfoActivity view) {
+        super(view);
+    }
+}
