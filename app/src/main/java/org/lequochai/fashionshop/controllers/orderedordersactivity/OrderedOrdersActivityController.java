ac0cb262dc283c41be33da6b48afae1ef75d4e79
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-14 13:38:00 +0700

    Added OrderedOrdersActivityController abstract class.

---
+++ app/src/main/java/org/lequochai/fashionshop/controllers/orderedordersactivity/OrderedOrdersActivityController.java
@@ -0,0 +1,11 @@
+package org.lequochai.fashionshop.controllers.orderedordersactivity;
+
+import org.lequochai.fashionshop.OrderedOrderActivity;
+import org.lequochai.fashionshop.controllers.ViewController;
+
+public abstract class OrderedOrdersActivityController<P> extends ViewController<OrderedOrderActivity, P> {
+//    Constructors:
+    public OrderedOrdersActivityController(OrderedOrderActivity view) {
+        super(view);
+    }
+}
