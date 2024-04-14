5d8b32ec51de2160df22d04ff40b178370b7c2c9
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-14 21:38:51 +0700

    Added RegisterActivityController abstract class.

---
+++ app/src/main/java/org/lequochai/fashionshop/controllers/registeractivity/RegisterActivityController.java
@@ -0,0 +1,11 @@
+package org.lequochai.fashionshop.controllers.registeractivity;
+
+import org.lequochai.fashionshop.RegisterActivity;
+import org.lequochai.fashionshop.controllers.ViewController;
+
+public abstract class RegisterActivityController<P> extends ViewController<RegisterActivity, P> {
+//    Constructors:
+    public RegisterActivityController(RegisterActivity view) {
+        super(view);
+    }
+}
